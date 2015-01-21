71e2a2a7e0d09ee43889dcf559531624876b519e
Fedor Indutny <fedor@indutny.com>
2015-01-21 20:30:55 +0300

    more progress

--- src/pyg.c
+++ src/pyg.c
@@ -1,5 +1,23 @@
-#include "json.h"
+#include "parson.h"
+
+#include <getopt.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
+  JSON_Value* json;
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage:\n  %s file.gyp\n", argv[0]);
+    return -1;
+  }
+
+  json = json_parse_file_with_comments(argv[1]);
+  if (json == NULL) {
+    fprintf(stderr, "Failed to parse JSON in %s\n", argv[1]);
+    return -1;
+  }
+
+  json_value_free(json);
+
   return 0;
 }
