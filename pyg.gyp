a5846a1a80c7823e293156ae6c9e4ad7a5b02e7a
Fedor Indutny <fedor@indutny.com>
2015-01-25 00:04:01 +0300

    ninja: small improvements

--- pyg.gyp
+++ pyg.gyp
@@ -1,7 +1,21 @@
 {
   "targets": [{
-    "target_name": "pyg"
+    "target_name": "pyg",
     "type": "executable",
 
+    "include_dirs": [
+      ".",
+      "deps/parson",
+    ],
+
+    "cflags": "-g3 -O0 -std=c99 -Wall -Wextra -Wno-unused-parameter -pedantic",
+
+    "sources": [
+      "src/common.c",
+      "src/error.c",
+      "src/pyg.c",
+      "src/generator/ninja.c",
+      "src/cli.c",
+    ]
   }],
 }
--- src/generator/ninja.c
+++ src/generator/ninja.c
@@ -2,6 +2,14 @@
 #include "src/common.h"
 #include "src/pyg.h"
 
+#define CHECKED_PRINT(...)                                                    \
+    do {                                                                      \
+      pyg_error_t err;                                                        \
+      err = pyg_buf_put(state->out, __VA_ARGS__);                             \
+      if (!pyg_is_ok(err))                                                    \
+        return err;                                                           \
+    } while (0)
+
 static pyg_error_t pyg_gen_ninja_target_cb(pyg_state_t* state,
                                            pyg_target_t* target);
 
@@ -12,11 +20,11 @@ pyg_gen_t pyg_gen_ninja = {
 
 pyg_error_t pyg_gen_ninja_target_cb(pyg_state_t* state,
                                     pyg_target_t* target) {
-  if (target->pyg->id == 0)
-    return pyg_buf_put(state->out, "build %s: phony\n", target->name);
+  CHECKED_PRINT("build %d__%s: phony\n", target->pyg->id, target->name);
 
-  return pyg_buf_put(state->out,
-                     "build %d__%s: phony\n",
-                     target->pyg->id,
-                     target->name);
+  /* Root targets should be reachable by plain name */
+  if (target->pyg->id == 0)
+    CHECKED_PRINT("build %s: phony 0__%s\n", target->name, target->name);
+
+  return pyg_ok();
 }
